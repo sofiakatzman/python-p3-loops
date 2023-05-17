a700758a526dd65230120f9d5e72a58278ca157f
Sofia Katzman <katzmansof@gmail.com>
2023-05-16 23:27:00 -0400

    finished lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10 
+    while i > 0: 
+        print(i)
+        i -= 1
+    print("Happy New Year!") 
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = []
+    for num in int_list:
+        squared_list.append(num ** 2)
+    return squared_list
 
 def fizzbuzz():
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+                  
+
     # code goes here!
     pass
